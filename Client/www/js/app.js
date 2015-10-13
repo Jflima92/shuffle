039602eda6d418a6d8f0463ae2c1e704c76ffed9
Jorge Lima <ei10102@fe.up.pt>
2015-10-14 01:28:23 +0200

    facebook login working on device,  but not working on browser

--- Client/www/js/app.js
+++ Client/www/js/app.js
@@ -7,112 +7,129 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('shuffle', ['ionic', 'shuffle.controllers', 'shuffle.services', 'btford.socket-io', 'ngCordova'])
 
-    .run(function($ionicPlatform, $cordovaFacebook) {
-      $ionicPlatform.ready(function() {
-        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-        // for form inputs)
-        if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-          cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-          cordova.plugins.Keyboard.disableScroll(true);
+    .run(function($ionicPlatform) {
+        $ionicPlatform.ready(function() {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                cordova.plugins.Keyboard.disableScroll(true);
 
+            }
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleLightContent();
+            }
+
+
+
+        });
+    })
+
+    .config(function($cordovaFacebookProvider){
+
+        //this.browserInit = function(id, version) {
+        //    if (!window.cordova) { //Comment for browser testing, uncomment to deploy
+        //        var appID = 367156356826931;
+        //        var version = "v2.0";
+        //        $cordovaFacebookProvider.browserInit(appID, version);
+        //    }
+        //}
+
+        this.browserInit = function(id, version) {
+            ionic.Platform.ready(function () {
+
+                var appID = 1476479009327244;
+                var version = "v2.0"; // or leave blank and default is v2.0
+
+                $cordovaFacebookProvider.browserInit(appID, version);
+            })
         }
-        if (window.StatusBar) {
-          // org.apache.cordova.statusbar required
-          StatusBar.styleLightContent();
-        }
-
-
-          var appID = 1476479009327244;
-          var version = "v2.0"; // or leave blank and default is v2.0
-
-          $cordovaFacebook.browserInit(appID, version);
-
-      });
     })
 
     .config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
 
-      // Ionic uses AngularUI Router which uses the concept of states
-      // Learn more here: https://github.com/angular-ui/ui-router
-      // Set up the various states which the app can be in.
-      // Each state's controller can be found in controllers.js
+        // Ionic uses AngularUI Router which uses the concept of states
+        // Learn more here: https://github.com/angular-ui/ui-router
+        // Set up the various states which the app can be in.
+        // Each state's controller can be found in controllers.js
 
-      $ionicConfigProvider
-          .tabs.position('bottom');
+        $ionicConfigProvider
+            .tabs.position('bottom');
 
-      $stateProvider
+        $stateProvider
 
-          .state('Intro', {
-            url: '/intro',
-            templateUrl: 'templates/intro.html',
-            controller: 'IntroCtrl'
-          })
+            .state('Intro', {
+                url: '/intro',
+                templateUrl: 'templates/intro.html',
+                controller: 'IntroCtrl'
+            })
 
-          .state('Settings', {
-              url: '/settings',
-              templateUrl: 'templates/settings.html',
-              controller: 'SettingsCtrl'
-          })
+            .state('Settings', {
+                url: '/settings',
+                templateUrl: 'templates/settings.html',
+                controller: 'SettingsCtrl'
+            })
 
-        // setup an abstract state for the tabs directive
-          .state('tab', {
-            url: '/tab',
-            abstract: true,
-            templateUrl: 'templates/tabs.html'
-          })
+            // setup an abstract state for the tabs directive
+            .state('tab', {
+                url: '/tab',
+                abstract: true,
+                templateUrl: 'templates/tabs.html'
+            })
 
-        // Each tab has its own nav history stack:
+            // Each tab has its own nav history stack:
 
-          .state('tab.mood', {
-            url: '/mood',
-            views: {
-              'tab-mood': {
-                templateUrl: 'templates/tab-mood.html',
-                controller: 'MoodCtrl'
-              }
-            }
-          })
+            .state('tab.mood', {
+                url: '/mood',
+                views: {
+                    'tab-mood': {
+                        templateUrl: 'templates/tab-mood.html',
+                        controller: 'MoodCtrl'
+                    }
+                }
+            })
 
-          .state('tab.routines', {
-            url: '/routines',
-            views: {
-              'tab-routines': {
-                templateUrl: 'templates/tab-routines.html',
-                controller: 'RoutinesCtrl'
-              }
-            }
-          })
+            .state('tab.routines', {
+                url: '/routines',
+                views: {
+                    'tab-routines': {
+                        templateUrl: 'templates/tab-routines.html',
+                        controller: 'RoutinesCtrl'
+                    }
+                }
+            })
 
-          /*.state('tab.chats', {
-            url: '/chats',
-            views: {
-              'tab-chats': {
-                templateUrl: 'templates/tab-chats.html',
-                controller: 'ChatsCtrl'
-              }
-            }
-          })
-          .state('tab.chat-detail', {
-            url: '/chats/:chatId',
-            views: {
-              'tab-chats': {
-                templateUrl: 'templates/chat-detail.html',
-                controller: 'ChatDetailCtrl'
-              }
-            }
-          })*/
+            /*.state('tab.chats', {
+             url: '/chats',
+             views: {
+             'tab-chats': {
+             templateUrl: 'templates/tab-chats.html',
+             controller: 'ChatsCtrl'
+             }
+             }
+             })
+             .state('tab.chat-detail', {
+             url: '/chats/:chatId',
+             views: {
+             'tab-chats': {
+             templateUrl: 'templates/chat-detail.html',
+             controller: 'ChatDetailCtrl'
+             }
+             }
+             })*/
 
-          .state('tab.weather', {
-            url: '/weather',
-            views: {
-              'tab-weather': {
-                templateUrl: 'templates/tab-weather.html',
-                controller: 'WeatherCtrl'
-              }
-            }
-          });
+            .state('tab.weather', {
+                url: '/weather',
+                views: {
+                    'tab-weather': {
+                        templateUrl: 'templates/tab-weather.html',
+                        controller: 'WeatherCtrl'
+                    }
+                }
+            });
 
-      // if none of the above states are matched, use this as the fallback
-      $urlRouterProvider.otherwise('/intro');
+        // if none of the above states are matched, use this as the fallback
+        $urlRouterProvider.otherwise('/intro');
 
     });
