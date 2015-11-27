3b9e570aab95dba3bfc006e220b72c0210308789
Joao Vieira de Castro <manueljoaocastro@gmail.com>
2015-11-27 17:51:12 +0100

    intro background image resize

--- Client/www/js/app.js
+++ Client/www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('shuffle', ['ionic', 'shuffle.controllers', 'shuffle.services', 'btford.socket-io', 'ngCordova', 'spotify', 'ionic-audio'])
+angular.module('shuffle', ['ionic', 'shuffle.controllers', 'shuffle.services', 'btford.socket-io', 'ngCordova', 'spotify'])
 
     .run(function($ionicPlatform, $cordovaGeolocation, geoLocation) {
         $ionicPlatform.ready(function() {
